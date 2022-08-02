af7cb98806c760860e161ee5f67f60128ddf0414
Bernard Agyeman <bernardagy22@gmail.com>
2022-08-02 12:14:01 -0400

    input component progress

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-const InputField = () => {
+//Creating object for useState types
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+//Created component with todo property
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  // user reference for input
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+    //creating input form, box and button
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        // gets rid of blur
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        value={todo}
+        //whenever inputting, set the todo to the provided input
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
