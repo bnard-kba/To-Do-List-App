8524be90b9ae6c9fad5a663ab36d67ea742c0e0d
Bernard Agyeman <bernardagy22@gmail.com>
2022-08-02 12:13:49 -0400

    created new components and event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./components/model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
-  const [toDo, setTodo] = useState<string>("");
+  //useState creation of string type
+  const [todo, setTodo] = useState<string>("");
+
+  //creating array of interface
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  //whenever submitting the form, take the event and add the task to the handler
+  const handleAdd = (e: React.FormEvent) => {
+    //prevent site from refreshing on form submittal
+    e.preventDefault();
+
+    //If there is something todo, set the array with the todo, an id#, and if it's done or not
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
+      {/*Creating heading for app */}
       <span className="heading">Taskify</span>
-      <InputField></InputField>
+      {/* Input component */}
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* Card component */}
+      <TodoList todos={todos} setTodos={setTodos} />
+
+      {/* <SingleTodo /> */}
     </div>
   );
 };
